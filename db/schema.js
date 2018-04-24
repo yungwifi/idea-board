c2b3850f928b596379a4c1390b37df29f95468fd
yungwifi <smerryman@me.com>
2018-04-24 12:13:16 -0400

    Seeded database

---
+++ db/schema.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+// First, we instantiate a namespace for our Schema constructor defined by mongoose.
+const Schema = mongoose.Schema
+
+const IdeaSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        default: "New Title"
+    },
+    description: {
+        type: String,
+        required: true,
+        default: "New Description"
+    },
+    created: {
+        type: String,
+        required: true,
+        default: new Date()
+    }
+})
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    ideas: [IdeaSchema]
+})
+
+// Create models for each schema
+const User = mongoose.model('User', UserSchema)
+const Idea = mongoose.model('Idea', IdeaSchema)
+
+// Export each model so they can be required elsewhere
+module.exports = {
+    User,
+    Idea
+}
